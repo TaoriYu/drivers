9527bff762a970d38e89ecb31f468e4405458cc2
Taori Yu <taoriyu@gmail.com>
2015-03-21 13:03:16 +0300

    ITS WORKING

--- test.c
+++ test.c
@@ -1,152 +1,138 @@
-#include <linux/kernel.h> 
-#include <linux/module.h> 
-#include <linux/init.h> 
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
 #include <linux/fs.h>
 #include <asm/uaccess.h>
 #include <linux/wait.h>
 #include <linux/sched.h>
 
-MODULE_LICENSE( "GPL" );
-MODULE_AUTHOR( "Taori & habrahabr :D" );
-MODULE_DESCRIPTION( "My test module" );
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Taori & habrahabr :D");
+MODULE_DESCRIPTION("My test module");
 
-static DECLARE_WAIT_QUEUE_HEAD( queue );
+static DECLARE_WAIT_QUEUE_HEAD(queue);
 
 #define DEVICE_NAME "test"
 
-static int device_open( struct inode *, struct file * );
-static int device_release( struct inode *, struct file * );
-static ssize_t device_read( struct file *, char *, size_t, loff_t * );
-static ssize_t device_write( struct file *, const char *, size_t, loff_t * );
+static int device_open(struct inode *, struct file *);
+static int device_release(struct inode *, struct file *);
+static ssize_t device_read(struct file *, char *, size_t, loff_t *);
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 
-static int flag = 0;
-static int major_number; /* Старший номер устройства нашего драйвера */
-static int are_we_reading = 0;
-static int are_we_writing = 0;
-static char text[ 100 ] = "my new text\n";
-static char* text_ptr = text; /* Указатель на текущую позицию в тексте */
+static int flag;
+static int major_number;
+static int are_we_reading;
+static int are_we_writing;
+static char text[100] = "my new text\n";
+static char *text_ptr = text;
 
 
-static struct file_operations fops =
-{
-    .read = device_read,
-    .write = device_write,
-    .open = device_open,
-    .release = device_release
+static const struct file_operations fops = {
+	.read = device_read,
+	.write = device_write,
+	.open = device_open,
+	.release = device_release
 };
 
 
 
-static int __init test_init( void )
+static int __init test_init(void)
 {
-    printk( KERN_ALERT "TEST driver loaded!\n" );
+	pr_alert("TEST driver loaded!\n");
 
-     // Регистрируем устройсво и получаем старший номер устройства
-     major_number = register_chrdev( 0, DEVICE_NAME, &fops );
+	major_number = register_chrdev(0, DEVICE_NAME, &fops);
 
-    if ( major_number < 0 )
-    {
-        printk( "Registering the character device failed with %d\n", major_number );
-        return major_number;
-    }
+	if (major_number < 0) {
 
-    printk( "Test module is loaded!\n" );
-    printk( "Please, create a dev file with 'mknod /dev/test c %d 0'.\n", major_number );
+		pr_alert("Registering failed with %d\n", major_number);
+		return major_number;
+	}
 
-    return 0;
+	pr_alert("Test module is loaded!\n");
+	pr_alert("Create a dev file with 'mknod /dev/test c %d 0'.\n", major_number);
+
+	return 0;
 }
 
 
 
-static void __exit test_exit( void )
+static void __exit test_exit(void)
 {
-    unregister_chrdev( major_number, DEVICE_NAME );
-    printk( KERN_ALERT "Test module is unloaded!\n" );
+	unregister_chrdev(major_number, DEVICE_NAME);
+	pr_alert("Test module is unloaded!\n");
 }
 
 
-module_init( test_init );
-module_exit( test_exit );
+module_init(test_init);
+module_exit(test_exit);
 
 
-static int device_open( struct inode *inode, struct file *file )
+static int device_open(struct inode *inode, struct file *file)
 {
-    text_ptr = text;
+	text_ptr = text;
 
-    if ( ( file->f_flags & O_ACCMODE ) == O_RDONLY)
-    {
+	if ((file->f_flags & O_ACCMODE) == O_RDONLY) {
 
-      if(are_we_reading)
-      {
-        printk("We are busy by reading\n");
-        return -EBUSY;
-      }
+		if (are_we_reading) {
 
-      are_we_reading = 1;
+		  pr_alert("We are busy by reading\n");
+		  return -EBUSY;
+		}
 
-    }
+		are_we_reading = 1;
 
-    if ( ( file->f_flags & O_ACCMODE ) == O_WRONLY )
-    {
+	}
 
-      if(are_we_writing)
-      {
-        printk("We are busy\n");
-        return -EBUSY;
-      }
+	if ((file->f_flags & O_ACCMODE) == O_WRONLY) {
 
-      are_we_writing = 1;
+		if (are_we_writing) {
 
-    }
+		  pr_alert("We are busy\n");
+		  return -EBUSY;
+		}
 
-    return 0;
+		are_we_writing = 1;
+
+	}
+
+	 return 0;
 }
 
 
-static int device_release( struct inode *inode, struct file *file )
+static int device_release(struct inode *inode, struct file *file)
 {
-    if ( ( file->f_flags & O_ACCMODE ) == O_WRONLY )
-    {
-      are_we_writing = 0;
-    }
+	if ((file->f_flags & O_ACCMODE) == O_WRONLY)
+		are_we_writing = 0;
 
-    if ( ( file->f_flags & O_ACCMODE ) == O_RDONLY )
-    {
-      are_we_reading = 0;
-    }
+	if ((file->f_flags & O_ACCMODE) == O_RDONLY)
+		are_we_reading = 0;
 
-    return 0;
+	return 0;
 }
 
 
-static ssize_t device_write( struct file *file, const char *buffer, size_t length, loff_t * off )
+static ssize_t device_write(struct file *file, const char *buffer, size_t length, loff_t *off)
 {
-    flag = 1;
-    text_ptr = text;
-    copy_from_user( text, buffer, length );
-    printk( KERN_ALERT "Good morning! :D\n" );
-    wake_up_interruptible( &queue );
-    return length;
+	flag = 1;
+	text_ptr = text;
+	copy_from_user(text, buffer, length);
+	pr_alert("Good morning! :D\n");
+	wake_up_interruptible(&queue);
+	return length;
 }
 
 
-static ssize_t device_read( struct file *file, char *buffer, size_t length, loff_t * offset )
+static ssize_t device_read(struct file *file, char *buffer, size_t length, loff_t *offset)
 {
-    ssize_t ret;
+	ssize_t ret;
 
-    if ( text_ptr == 0 ) wait_event_interruptible( queue, flag != 0 );
-    printk( KERN_ALERT "Good night\n" );  
-    ret = copy_to_user( buffer, text, length );
-    text_ptr = 0;
-    flag = 0;
+	if (text_ptr == 0)
+		wait_event_interruptible(queue, flag != 0);
 
- /*while ( length && *text_ptr )
- {
-  if ( put_user( *( text_ptr++ ), buffer++ ) ) 
-    return -EFAULT;
-  length--;
-  byte_read++;
- }*/
+	pr_alert("Good night\n");
+	ret = copy_to_user(buffer, text, length);
+	text_ptr = 0;
+	flag = 0;
 
-    return ret;
+	return ret;
 }
